d899a4d37ddef8575a5764541e9631a05a693d41
Code X Mania <urluploaderadarsh@gmail.com>
2021-11-12 11:59:07 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -1,20 +1,14 @@
-'''
-© Mrvishal2k2
-RenameBot
-This file is a part of mrvishal2k2 rename repo 
-Dont kang !!!
-© Mrvishal2k2
-'''
+
 import os 
 
 class Config(object):
   APP_ID = int(os.environ.get("APP_ID", ""))
   API_HASH = os.environ.get("API_HASH", "")
-  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
   DOWNLOAD_LOCATION = "./bot/DOWNLOADS"
   DB_URI = os.environ.get("DATABASE_URL", "")
   # owner is for log cmd only owner can use (this can be multiple users)
   OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
-  OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "BotDunia")
+  OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "adarsh")
   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",False)
